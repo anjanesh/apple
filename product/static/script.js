87e40d159c14e38208b705e7c1c88e4a908b41d7
Anjanesh Lekshminarayanan <anjanesh@gmail.com>
2022-07-20 08:07:29 +0530

    dropbox conflict update

---
+++ product/static/script.js
@@ -0,0 +1,36 @@
+let trs;
+document.addEventListener("DOMContentLoaded", () =>
+{
+    let tbl_products = document.getElementById('tbl-products');
+    let tbl_products_tbody_trs = tbl_products.querySelectorAll('tbody > tr');
+    trs = [...tbl_products_tbody_trs];        
+
+});      
+
+document.addEventListener("DOMContentLoaded", () =>
+{
+    document.getElementById('search').addEventListener('input', (e) =>
+    {
+        let search = e.target.value;
+        
+        trs.forEach(tr =>
+        {
+            let found = false;
+
+            [...tr.children].forEach(child =>
+            {
+                if (child.localName == 'th') return;
+                let cell = child.innerHTML.trim();                    
+
+                if (cell.toLowerCase().includes(search.toLowerCase()))
+                {
+                    found = true;
+                    return;
+                }
+            });
+            
+            tr.style.display = found ? 'table-row' : 'none';
+            
+        });
+    });
+});
